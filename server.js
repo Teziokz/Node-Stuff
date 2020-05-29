ae4a769befd39e5670a20df01e95be2d9ad24f4f
Glitch (hello-express) <none>
2020-05-29 17:07:17 +0100

    Removed some of the more compex stuff

--- server.js
+++ server.js
@@ -5,13 +5,7 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const port = process.env.PORT || 3000;
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -22,13 +16,7 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(port, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
