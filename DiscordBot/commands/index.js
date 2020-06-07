99b6bb287d4dad93c70f6d828492282a5b03e5aa
Glitch (teziokz-node-stuff) <none>
2020-06-07 11:57:06 +0000

    Before import of Teziokz/Node-Stuff without specified path ./.gitignore:16886889/20 ./DiscordBot/commandHistory.json:16886889/27

--- DiscordBot/commands/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Ping: require("./ping"),
-  Joke: require("./joke"),
-  Sound: require("./sounds"),
-  Clear: require("./clear-chat"),
-  Poll: require("./poll"),
-  Help: require("./help"),
-  History: require("./history"),
-};
